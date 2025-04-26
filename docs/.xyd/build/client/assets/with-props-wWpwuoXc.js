370fca549b6b6cc4be220d5b2e63a4805fa60b3c
Patryk Zdunowski <zdunekhere@gmail.com>
2025-04-26 15:14:17 +0200

    added build

---
+++ docs/.xyd/build/client/assets/with-props-wWpwuoXc.js
@@ -0,0 +1,1 @@
+import{u as e,b as r,c as s,d as o,r as n}from"./chunk-LSOULM7L-ZsbqoCmN.js";function u(a){return function(){const t={params:o(),loaderData:s(),actionData:r(),matches:e()};return n.createElement(a,t)}}export{u as w};
